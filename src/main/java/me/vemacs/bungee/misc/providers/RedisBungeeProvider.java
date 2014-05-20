f4032bd544bfa2ca5c486c8a45a5df9184a0f3ac
vemacs <gentoo@outlook.com>
2014-05-20 17:03:29 -0600

    Update to RedisBungee 0.3, bump version to 1.3

--- src/main/java/me/vemacs/bungee/misc/providers/RedisBungeeProvider.java
+++ src/main/java/me/vemacs/bungee/misc/providers/RedisBungeeProvider.java
@@ -3,7 +3,9 @@ package me.vemacs.bungee.misc.providers;
 import com.imaginarycode.minecraft.redisbungee.RedisBungee;
 import com.imaginarycode.minecraft.redisbungee.RedisBungeeAPI;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class RedisBungeeProvider implements ServerInfoProvider {
     private static RedisBungeeAPI redisBungee = RedisBungee.getApi();
@@ -15,6 +17,9 @@ public class RedisBungeeProvider implements ServerInfoProvider {
 
     @Override
     public Set<String> getPlayersOnServer(String server) {
-        return redisBungee.getPlayersOnServer(server);
+        Set<String> humanPlayersOnServer = new HashSet<>();
+        for (UUID uuid : redisBungee.getPlayersOnServer(server))
+            humanPlayersOnServer.add(redisBungee.getNameFromUuid(uuid));
+        return humanPlayersOnServer;
     }
 }
